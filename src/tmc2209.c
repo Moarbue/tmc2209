dd033710dac09f6a790381b3772467ec693bb6f1
Moarbue <83272310+Moarbue@users.noreply.github.com>
2023-03-16 12:13:54 +0100

    Implement functions except moving move ones

---
+++ src/tmc2209.c
@@ -0,0 +1,150 @@
+#include "tmc2209.h"
+
+static bool serial_initialized = false;
+HardwareSerial s_serial(TMC2209_DEFAULT_SERIAL);
+
+// helper functions declaration
+void set_adress(tmc2209_t *s, tmc2209_adress adress);
+
+void tmc2209_full(tmc2209_t *s, uint8_t en_pin, uint8_t dir_pin, uint8_t step_pin,
+                                      uint8_t rx_pin, uint8_t tx_pin,  uint8_t ms1_pin, uint8_t ms2_pin,
+                                      tmc2209_adress adress)
+{
+    if (s == NULL) return;
+
+    memset(s, 0, sizeof (*s));
+
+    s->en_pin               = en_pin;
+    s->dir_pin              = dir_pin;
+    s->step_pin             = step_pin;
+    s->rx_pin               = rx_pin;
+    s->tx_pin               = tx_pin;
+    s->ms1_pin              = ms1_pin;
+    s->ms2_pin              = ms2_pin;
+
+    s->adress               = adress;
+    s->microsteps           = TMC2209_DEFAULT_MICROSTEPS;
+    s->dir                  = TMC2209_CW;
+    s->step_delay           = TMC2209_DEFAULT_STEP_DELAY;
+    s->steps_per_revolution = TMC2209_DEFAULT_STEPS_PER_REVOLUTION;
+
+    s->step                 = 0;
+    s->steps                = 0;
+
+    pinMode(s->en_pin, OUTPUT);
+    // disable driver
+    digitalWrite(s->en_pin, HIGH);
+
+    pinMode(s->dir_pin,  OUTPUT);
+    pinMode(s->step_pin, OUTPUT);
+    pinMode(s->ms1_pin,  OUTPUT);
+    pinMode(s->ms2_pin,  OUTPUT);
+
+    // initialize UART
+    if (serial_initialized) return;
+    s_serial.begin(115200, SERIAL_8N1, s->rx_pin, s->tx_pin);
+    serial_initialized = true;
+}
+
+void tmc2209_set_microsteps(tmc2209_t *s, tmc2209_microstep microsteps)
+{
+    if (s == NULL) return;
+
+    // TODO: Implement Error Reporting
+    if (microsteps != TMC2209_MICROSTEPS_8  || microsteps != TMC2209_MICROSTEPS_16 ||
+        microsteps != TMC2209_MICROSTEPS_32 || microsteps != TMC2209_MICROSTEPS_64)
+        return;
+
+    s->microsteps = microsteps;
+
+    if (serial_initialized) {
+        // TODO: set microsteps via UART
+    } else {
+        switch (s->microsteps) {
+            case TMC2209_MICROSTEPS_8:
+                digitalWrite(s->ms1_pin, LOW);
+                digitalWrite(s->ms2_pin, LOW);
+            break;
+            case TMC2209_MICROSTEPS_16:
+                digitalWrite(s->ms1_pin, HIGH);
+                digitalWrite(s->ms2_pin, HIGH);
+            break;
+            case TMC2209_MICROSTEPS_32:
+                digitalWrite(s->ms1_pin, HIGH);
+                digitalWrite(s->ms2_pin, LOW);
+            break;
+            case TMC2209_MICROSTEPS_64:
+                digitalWrite(s->ms1_pin, LOW);
+                digitalWrite(s->ms2_pin, HIGH);
+            break;
+        }
+    }
+}
+
+tmc2209_microstep tmc2209_get_microsteps(tmc2209_t *s)
+{
+    if (s == NULL) return TMC2209_DEFAULT_MICROSTEPS;
+
+    return s->microsteps;
+}
+
+void tmc2209_set_step_delay(tmc2209_t *s, uint32_t step_delay_us)
+{
+    if (s == NULL) return;
+
+    s->step_delay = step_delay_us;
+}
+
+uint32_t tmc2209_get_step_delay(tmc2209_t *s)
+{
+    if (s == NULL) return TMC2209_DEFAULT_STEP_DELAY;
+
+    return s->step_delay;
+}
+
+void tmc2209_set_direction(tmc2209_t *s, tmc2209_direction dir)
+{
+    if (s == NULL) return;
+
+    if (dir != TMC2209_CW || dir != TMC2209_CCW) return;
+
+    if (serial_initialized) {
+        // TODO: Implement setting direction via UART
+    } else 
+        digitalWrite(s->dir_pin, s->dir);
+}
+
+tmc2209_direction tmc2209_get_direction(tmc2209_t *s)
+{
+    if (s == NULL) return TMC2209_CCW;
+
+    return s->dir; 
+}
+
+
+// helper functions definition
+
+void set_adress(tmc2209_t *s, tmc2209_adress adress)
+{
+    switch (adress) {
+        case TMC2209_ADRESS_0:
+            digitalWrite(s->ms1_pin, LOW);
+            digitalWrite(s->ms2_pin, LOW);
+        break;
+        case TMC2209_ADRESS_1:
+            digitalWrite(s->ms1_pin, HIGH);
+            digitalWrite(s->ms2_pin, LOW);
+        break;
+        case TMC2209_ADRESS_2:
+            digitalWrite(s->ms1_pin, LOW);
+            digitalWrite(s->ms2_pin, HIGH);
+        break;
+        case TMC2209_ADRESS_3:
+            digitalWrite(s->ms1_pin, HIGH);
+            digitalWrite(s->ms2_pin, HIGH);
+        break;
+        default:
+            // TODO: Implement Error Reporting
+            assert(false && "Wrong Adress specified!");
+    }
+}
--- src/tmc2209.h
+++ src/tmc2209.h
@@ -1,4 +1,4 @@
-// #include "Arduino.h"
+#include "Arduino.h"
 
 #ifndef TMC2209_H_
 #define TMC2209_H_
@@ -6,11 +6,6 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-typedef unsigned char uint8_t;
-typedef short uint16_t;
-typedef unsigned int uint32_t;
-typedef int int32_t;
 
 typedef enum {
     TMC2209_ADRESS_0  = 0x0b00,
@@ -44,6 +39,10 @@ typedef enum {
 #define TMC2209_DEFAULT_STEP_DELAY 1000 // µs
 #endif
 
+#ifndef TMC2209_DEFAULT_SERIAL
+#define TMC2209_DEFAULT_SERIAL 1
+#endif
+
 typedef struct {
     uint8_t en_pin;     // enable
     uint8_t dir_pin;    // direction
@@ -55,6 +54,7 @@ typedef struct {
 
     tmc2209_adress    adress;               // adress for UART (see enum)
     tmc2209_microstep microsteps;           // divisor for single step (see enum)
+    tmc2209_direction dir;                  // spinning direction of the motor
     uint32_t          step_delay;           // delay in µs between one low/high pulse
     uint16_t          steps_per_revolution; // this number * microsteps = steps for one full turn
 
@@ -63,36 +63,36 @@ typedef struct {
 } tmc2209_t;
 
 // initialize the driver, both interfaces
-void tmc2209_full(tmc2209_t *stepper, uint8_t en_pin, uint8_t dir_pin, uint8_t step_pin,
+void tmc2209_full(tmc2209_t *s, uint8_t en_pin, uint8_t dir_pin, uint8_t step_pin,
                                       uint8_t rx_pin, uint8_t tx_pin,  uint8_t ms1_pin, uint8_t ms2_pin,
                                       tmc2209_adress adress);
 
 // set the microsteps of the driver
-void tmc2209_set_microsteps(tmc2209_t *stepper, tmc2209_microstep microsteps);
+void tmc2209_set_microsteps(tmc2209_t *s, tmc2209_microstep microsteps);
 // get the microsteps of the driver
-tmc2209_microstep tmc2209_get_microsteps(tmc2209_t *stepper);
+tmc2209_microstep tmc2209_get_microsteps(tmc2209_t *s);
 
 // set the step delay between one high/low pulse in µs
-void tmc2209_set_step_delay(tmc2209_t *stepper, uint32_t step_delay_us);
+void tmc2209_set_step_delay(tmc2209_t *s, uint32_t step_delay_us);
 // get the step delay between one high/low pulse in µs
-uint32_t tmc2209_get_step_delay(tmc2209_t *stepper);
+uint32_t tmc2209_get_step_delay(tmc2209_t *s);
 
 // set the spin-direction of the motor
-void tmc2209_set_direction(tmc2209_t *stepper, tmc2209_direction dir);
+void tmc2209_set_direction(tmc2209_t *s, tmc2209_direction dir);
 // get the spin-direction of the motor
-tmc2209_direction tmc2209_get_direction(tmc2209_t *stepper);
+tmc2209_direction tmc2209_get_direction(tmc2209_t *s);
 
 
 // functions for moving the motor via STEP/DIR interface
 
 // move the motor by steps and direction
-void tmc2209_step(tmc2209_t *stepper, uint32_t steps, tmc2209_direction dir);
+void tmc2209_step(tmc2209_t *s, uint32_t steps, tmc2209_direction dir);
 
 // move the motor by degrees, sign specifies the direction
-void tmc2209_rotate(tmc2209_t *stepper, int32_t degree);
+void tmc2209_rotate(tmc2209_t *s, int32_t degree);
 
 // all functions are non blocking, therefore call update function every loop iteration
-void tmc2209_update(tmc2209_t *stepper);
+void tmc2209_update(tmc2209_t *s);
 
 
 #ifdef __cplusplus
